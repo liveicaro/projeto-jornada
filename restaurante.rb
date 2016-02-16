8b4dd7fbcdf018933bb3a960e865d9580b84d10f
citi <citi@ug4c12.windows.cin.ufpe.br>
2016-02-16 11:46:11 -0300

    restau

---
+++ restaurante.rb
@@ -0,0 +1,24 @@
+	
+	class Restaurante
+	 attr_accessor :nota
+	 
+	 def initialize (nome)
+	  puts "criando um novo restaurante: #{nome}"
+	  @nome = nome
+	 end
+
+	 def qualifica (msg="Obrigado")
+	  puts "A nota do #{@nome} foi #{@nota}. #{msg}"
+	 end
+	end
+
+restaurante_um = Restaurante.new ("Fasano")
+restaurante_dois = Restaurante.ew("Fogo de Chao")
+
+restaurante_um.nota = 10
+restaurante_dois.nota = 1
+
+restaurante_um.qualifica
+restaurante_dois.qualifica("comida ruim.")
+
+
